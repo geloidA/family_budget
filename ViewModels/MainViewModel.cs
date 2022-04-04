769d7b2e04dbd8d5acbfe803fc8ce54c9bc57af6
Aleksandr <alexkolpak2003@gmail.com>
2022-04-04 12:07:39 +0500

    Add project files.

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace family_budget.ViewModels
+{
+    class MainViewModel : ViewModelBase
+    { 
+        private int _clicks;
+        public int Clicks
+        {
+            get { return _clicks; }
+            set 
+            {
+                _clicks = value;
+                RaisePropertyChanged(() => Clicks);
+            }
+        }
+
+        public ICommand ClickAdd
+        {
+            get
+            {
+                return new DelegateCommand(
+                    () => Clicks++, () => Clicks < 10);
+            }
+        }
+    }
+}
