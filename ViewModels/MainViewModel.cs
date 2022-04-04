87f8ffae8b146437622601f1336ac67e3d28e9f7
Aleksandr <alexkolpak2003@gmail.com>
2022-04-04 12:50:06 +0500

    Some changes (don't push)

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -29,5 +29,14 @@ namespace family_budget.ViewModels
                     () => Clicks++, () => Clicks < 10);
             }
         }
+
+        public ICommand ClickRemove
+        {
+            get
+            {
+                return new DelegateCommand(
+                    () => Clicks--);
+            }
+        }
     }
 }
